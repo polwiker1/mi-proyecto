000be5708a8bae6b40dd7c9580544bdb71e3c886
polwiker1 <cervezawicca@hotmail.com.ar>
2026-04-09 21:01:59 -0300

    Format Solidity files for CI

--- src/calculadora.sol
+++ src/calculadora.sol
@@ -1,53 +1,53 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.24;
- contract Calculadora {
 
+contract Calculadora {
     //variables
-   uint256 public resultado;
-   address public admin;
-//events
+    uint256 public resultado;
+    address public admin;
+    //events
     event Addition(uint256 firstNumber_, uint256 secondNumber_, uint256 resultado_);
     event Subtraction(uint256 firstNumber_, uint256 secondNumber_, uint256 resultado_);
     event Multiplication(uint256 firstNumber_, uint256 secondNumber_, uint256 resultado_);
     event Division(uint256 firstNumber_, uint256 secondNumber_, uint256 resultado_);
-//modifiers
+    //modifiers
     modifier onlyAdmin() {
         require(msg.sender == admin, "Not allowed");
         _;
     }
-   constructor(uint256 firstResult_, address admin_) {
-     resultado = firstResult_;
+
+    constructor(uint256 firstResult_, address admin_) {
+        resultado = firstResult_;
         admin = admin_;
-   }
-   //funcion suma
-//addition
-   function addition(uint256   firstNumber_, uint256 secondNumber_) external returns (uint256 resultado_) {
-     resultado_ = firstNumber_ + secondNumber_;
-    resultado = resultado_;
-   emit Addition(firstNumber_, secondNumber_, resultado_);
+    }
 
+    //funcion suma
+    //addition
+    function addition(uint256 firstNumber_, uint256 secondNumber_) external returns (uint256 resultado_) {
+        resultado_ = firstNumber_ + secondNumber_;
+        resultado = resultado_;
+        emit Addition(firstNumber_, secondNumber_, resultado_);
+    }
 
-   }
-   
+    //funcion sustraccion
+    function subtraction(uint256 firstNumber_, uint256 secondNumber_) external returns (uint256 resultado_) {
+        resultado_ = firstNumber_ - secondNumber_;
+        resultado = resultado_;
+        emit Subtraction(firstNumber_, secondNumber_, resultado_);
+        //multiplicacion
+    }
 
-   //funcion sustraccion 
-  function subtraction(uint256   firstNumber_, uint256 secondNumber_) external returns (uint256 resultado_ ) {
-     resultado_ = firstNumber_ - secondNumber_;
-    resultado = resultado_;
-   emit Subtraction(firstNumber_, secondNumber_, resultado_);
-//multiplicacion
-   }
-   function multiplication(uint256   firstNumber_, uint256 secondNumber_) external returns (uint256 resultado_ ) {
-     resultado_ = firstNumber_ * secondNumber_;
-    resultado = resultado_;
-   emit Multiplication(firstNumber_, secondNumber_, resultado_);
-//division
-   }
-   function division(uint256   firstNumber_, uint256 secondNumber_) external onlyAdmin returns (uint256 resultado_ ) {
-     resultado_ = firstNumber_ / secondNumber_;
-    resultado = resultado_;
-   emit Division(firstNumber_, secondNumber_, resultado_);
+    function multiplication(uint256 firstNumber_, uint256 secondNumber_) external returns (uint256 resultado_) {
+        resultado_ = firstNumber_ * secondNumber_;
+        resultado = resultado_;
+        emit Multiplication(firstNumber_, secondNumber_, resultado_);
+        //division
+    }
 
- }
- }
+    function division(uint256 firstNumber_, uint256 secondNumber_) external onlyAdmin returns (uint256 resultado_) {
+        resultado_ = firstNumber_ / secondNumber_;
+        resultado = resultado_;
+        emit Division(firstNumber_, secondNumber_, resultado_);
+    }
+}
--- test/CalculadoraTets.t.sol
+++ test/CalculadoraTets.t.sol
@@ -2,119 +2,112 @@
 
 pragma solidity ^0.8.24;
 
-
 import "../src/calculadora.sol";
 import "forge-std/Test.sol";
 
- contract CalculadoraTests is Test {
+contract CalculadoraTests is Test {
     Calculadora calculadora;
     uint256 firstResultado = 100;
     address public admin = vm.addr(1);
     address public randomUser = vm.addr(2);
 
-
     function setUp() public {
-calculadora = new Calculadora(firstResultado, admin);
-
+        calculadora = new Calculadora(firstResultado, admin);
     }
+
     //unit testing
     function testCheckFirstResultado() public view {
         uint256 firstResultado_ = calculadora.resultado();
         assertEq(firstResultado_, firstResultado);
- }
+    }
+
     function testAddition() public {
+        uint256 firstNumber_ = 5;
+        uint256 secondNumber_ = 5;
+        uint256 resultado_ = calculadora.addition(firstNumber_, secondNumber_);
 
-     uint256 firstNumber_ = 5;
-     uint256 secondNumber_ = 5;
-     uint256 resultado_ = calculadora.addition(firstNumber_, secondNumber_);
-    
-    assertEq(resultado_, firstNumber_ + secondNumber_);
+        assertEq(resultado_, firstNumber_ + secondNumber_);
+    }
 
- }
+    function testSubtraction() public {
+        uint256 firstNumber_ = 10;
+        uint256 secondNumber_ = 5;
+        uint256 resultado_ = calculadora.subtraction(firstNumber_, secondNumber_);
 
-     function testSubtraction() public {
+        assertEq(resultado_, firstNumber_ - secondNumber_);
+    }
 
-     uint256 firstNumber_ = 10;
-     uint256 secondNumber_ = 5;
-     uint256 resultado_ = calculadora.subtraction(firstNumber_, secondNumber_);
-    
-    assertEq(resultado_, firstNumber_ - secondNumber_);
-
- }
     function testMultiplication() public {
+        uint256 firstNumber_ = 10;
+        uint256 secondNumber_ = 5;
+        uint256 resultado_ = calculadora.multiplication(firstNumber_, secondNumber_);
 
+        assertEq(resultado_, firstNumber_ * secondNumber_);
+    }
 
-     uint256 firstNumber_ = 10;
-     uint256 secondNumber_ = 5;
-     uint256 resultado_ = calculadora.multiplication(firstNumber_, secondNumber_);
-    
-    assertEq(resultado_, firstNumber_ * secondNumber_);
- }
-function testCanNotMultiply2LargeNumbers() public {
+    function testCanNotMultiply2LargeNumbers() public {
+        uint256 firstNumber_ = type(uint256).max;
+        uint256 secondNumber_ = 5;
+        vm.expectRevert();
+        calculadora.multiplication(firstNumber_, secondNumber_);
+    }
 
-     uint256 firstNumber_ = type(uint256).max;
-     uint256 secondNumber_ = 5;
-     vm.expectRevert();
-     calculadora.multiplication(firstNumber_, secondNumber_);
- }
-function testCanNotAdminCallsDivisionRevert() public {
+    function testCanNotAdminCallsDivisionRevert() public {
+        vm.startPrank(randomUser);
 
-     vm.startPrank(randomUser);
-    
-     uint256 firstNumber_ = 5;
-     uint256 secondNumber_ = 2;
-     vm.expectRevert();
-     calculadora.division(firstNumber_, secondNumber_);
-     vm.stopPrank();
- }
-function testCanNotAdminCallsDivisionCorrectly() public {
+        uint256 firstNumber_ = 5;
+        uint256 secondNumber_ = 2;
+        vm.expectRevert();
+        calculadora.division(firstNumber_, secondNumber_);
+        vm.stopPrank();
+    }
 
-     vm.startPrank(admin);
-    
-     uint256 firstNumber_ = 5;
-     uint256 secondNumber_ = 2;
-      calculadora.division(firstNumber_, secondNumber_);
-     uint256 resultado_ = calculadora.resultado();
-     assertEq(resultado_, firstNumber_ / secondNumber_);
-     vm.stopPrank();
+    function testCanNotAdminCallsDivisionCorrectly() public {
+        vm.startPrank(admin);
 
- }
-function testCanNotAdminCallsDivisionByZero() public {
+        uint256 firstNumber_ = 5;
+        uint256 secondNumber_ = 2;
+        calculadora.division(firstNumber_, secondNumber_);
+        uint256 resultado_ = calculadora.resultado();
+        assertEq(resultado_, firstNumber_ / secondNumber_);
+        vm.stopPrank();
+    }
 
-     vm.startPrank(admin);
-    
-     uint256 firstNumber_ = 5;
-     uint256 secondNumber_ = 0;
-     vm.expectRevert();
-      calculadora.division(firstNumber_, secondNumber_);
-     vm.stopPrank();
-}
+    function testCanNotAdminCallsDivisionByZero() public {
+        vm.startPrank(admin);
 
-function testDefaultCanNotCallsDivisionCorrectly() public {
-     uint256 firstNumber_ = 5;
-     uint256 secondNumber_ = 2;
-     vm.expectRevert(bytes("Not allowed"));
-     calculadora.division(firstNumber_, secondNumber_);
-}
-function testDefaultExecutesCorrectly() public {
-    vm.startPrank(admin);
-     uint256 firstNumber_ = 5;
-     uint256 secondNumber_ = 2;
-     uint256 resultado_ = calculadora.division(firstNumber_, secondNumber_);
-     assertEq(resultado_, firstNumber_ / secondNumber_);
-     vm.stopPrank();
-}
+        uint256 firstNumber_ = 5;
+        uint256 secondNumber_ = 0;
+        vm.expectRevert();
+        calculadora.division(firstNumber_, secondNumber_);
+        vm.stopPrank();
+    }
 
-//fuzzing testing
-function testFuzzDivision(uint256 firstNumber_, uint256 secondNumber_) public {
-    vm.assume(secondNumber_ != 0);
+    function testDefaultCanNotCallsDivisionCorrectly() public {
+        uint256 firstNumber_ = 5;
+        uint256 secondNumber_ = 2;
+        vm.expectRevert(bytes("Not allowed"));
+        calculadora.division(firstNumber_, secondNumber_);
+    }
 
-    vm.startPrank(admin);
-    uint256 resultado_ = calculadora.division(firstNumber_, secondNumber_);
-    vm.stopPrank();
+    function testDefaultExecutesCorrectly() public {
+        vm.startPrank(admin);
+        uint256 firstNumber_ = 5;
+        uint256 secondNumber_ = 2;
+        uint256 resultado_ = calculadora.division(firstNumber_, secondNumber_);
+        assertEq(resultado_, firstNumber_ / secondNumber_);
+        vm.stopPrank();
+    }
 
-    assertEq(resultado_, firstNumber_ / secondNumber_);
-    assertEq(calculadora.resultado(), firstNumber_ / secondNumber_);
-}
+    //fuzzing testing
+    function testFuzzDivision(uint256 firstNumber_, uint256 secondNumber_) public {
+        vm.assume(secondNumber_ != 0);
 
+        vm.startPrank(admin);
+        uint256 resultado_ = calculadora.division(firstNumber_, secondNumber_);
+        vm.stopPrank();
+
+        assertEq(resultado_, firstNumber_ / secondNumber_);
+        assertEq(calculadora.resultado(), firstNumber_ / secondNumber_);
+    }
 }
